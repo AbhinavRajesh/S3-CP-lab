33a698b756c99a7116aeb03d669f87f2aa9763d4
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2020-11-05 11:51:16 +0530

    Removed the functions from the class

--- Program-7-swap.cpp
+++ Program-7-swap.cpp
@@ -13,43 +13,44 @@ public:
         cout << "\n Values of variable: \n"
              << " a = " << a << "\n b = " << b;
     }
-    void swapValue(int a, int b)
-    {
-        int temp = a;
-        a = b;
-        b = temp;
-        cout << "\n Value of variables after swap in swapValue() function [ Call By Value Method ] \n";
-        cout << " a = " << a << "\n b = " << b;
-    }
-
-    void swapReference(int &a, int &b)
-    {
-        int temp = a;
-        a = b;
-        b = temp;
-        cout << "\n Value of variables after swap in swapReference() function [ Call By Reference Method ] \n";
-        cout << " a = " << a << "\n b = " << b;
-    }
-
-    void swapAddress(int *a, int *b)
-    {
-
-        int temp = *a;
-        *a = *b;
-        *b = temp;
-        cout << "\n Value of variables after swap in swapAddress() function [ Call By Address Method ] \n";
-        cout << " a = " << *a << "\n b = " << *b;
-    }
 };
+
+void swapValue(int a, int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+    cout << "\n Value of variables after swap in swapValue() function [ Call By Value Method ] \n";
+    cout << " a = " << a << "\n b = " << b;
+}
+
+void swapReference(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+    cout << "\n Value of variables after swap in swapReference() function [ Call By Reference Method ] \n";
+    cout << " a = " << a << "\n b = " << b;
+}
+
+void swapAddress(int *a, int *b)
+{
+
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    cout << "\n Value of variables after swap in swapAddress() function [ Call By Address Method ] \n";
+    cout << " a = " << *a << "\n b = " << *b;
+}
 
 int main()
 {
     int temp;
     Swap s;
-    s.swapValue(s.a, s.b);
+    swapValue(s.a, s.b);
     cout << "\n Values of variables in Main after Call by Value Method \n";
     cout << " a = " << s.a << "\n b = " << s.b;
-    s.swapReference(s.a, s.b);
+    swapReference(s.a, s.b);
     cout << "\n Values of variables in Main after Call by Reference Method \n";
     cout << " a = " << s.a << "\n b = " << s.b;
     temp = s.a;
@@ -57,7 +58,7 @@ int main()
     s.b = temp;
     cout << "\n Values of variable: \n"
          << " a = " << s.a << "\n b = " << s.b;
-    s.swapAddress(&s.a, &s.b);
+    swapAddress(&s.a, &s.b);
     cout << "\n Values of variables in Main after Call by Address Method \n";
     cout << " a = " << s.a << "\n b = " << s.b;
 }
