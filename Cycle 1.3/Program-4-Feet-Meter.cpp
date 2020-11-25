2cfb1ad09f22438cd52bcffe4c8bee0904afdd4e
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2020-11-25 20:26:44 +0530

    Fixed 4th question

--- Cycle 1.3/Program-4-Feet-Meter.cpp
+++ Cycle 1.3/Program-4-Feet-Meter.cpp
@@ -14,6 +14,17 @@ public:
         cout << "\n Enter Inch: ";
         cin >> inch;
     }
+    Distance(float meter)
+    {
+        inch = (meter * 100) / 2.54;
+        feet = inch / 12;
+        inch = inch % 12;
+    }
+    void show()
+    {
+        cout << "\n Feet: " << feet;
+        cout << "\n Inch: " << inch;
+    }
     operator float()
     {
         return (((feet * 12 + inch) * 2.54) / 100);
@@ -22,8 +33,13 @@ public:
 
 int main()
 {
+    float input;
     Distance D;
     float meter = D;
     cout << "\n Convertion of Feet and inch distance to Meter: " << meter << "m";
+    cout << "\n Enter the distance in Meter to convert to Feet and Inch: ";
+    cin >> input;
+    Distance D2 = input;
+    D2.show();
     return 0;
 }
